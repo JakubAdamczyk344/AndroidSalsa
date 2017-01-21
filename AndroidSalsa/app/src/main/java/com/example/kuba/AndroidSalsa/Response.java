30d34babcaac3c753c3b3be73c1f2b0b3d446e93
Jakub Adamczyk <jakub.adamczyk344@gmail.com>
2017-01-21 21:24:50 +0100

    Dodano komentarze w klasie Response

--- AndroidSalsa/app/src/main/java/com/example/kuba/AndroidSalsa/Response.java
+++ AndroidSalsa/app/src/main/java/com/example/kuba/AndroidSalsa/Response.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Response {
-
+    //definicja listy przechowującej informacje o wydarzeniach
     private List<EventDescription> event = new ArrayList<EventDescription>();
-
+    //getter do pobierania wydarzenia
     public List<EventDescription> getEvent() {
         return event;
     }
